1f1f2e810fb595eaf17e83351a70f1e824a8e808
Victor Lelis <37216112+victorlelissoares@users.noreply.github.com>
2022-01-08 16:59:12 -0400

    Update onemax.cpp

--- onemax.cpp
+++ onemax.cpp
@@ -19,8 +19,10 @@ class Individuo{
 	public:
 		Individuo();
 		~Individuo();
+		void at_score();
 		int return_score();
 		void print_genes();
+		int operator<(Individuo &other);
 
 	//private:
 		vector<int> genes;//vetor de genes
@@ -28,6 +30,19 @@ class Individuo{
 		void mutation();//mutação
 		void crossing(Individuo pai1, Individuo pai2);//operador de cruzamento
 };
+
+void Individuo::at_score(){
+	this->score = 0;
+	for (int i = 0; i < tam_genes; ++i){
+		
+		if(this->genes[i] == 1)
+			score += 1;
+	}
+}
+
+int Individuo::operator<(Individuo &other){
+	return this->score < other.score;
+}
 
 //construtor
 Individuo::Individuo(){
@@ -88,9 +103,11 @@ void Individuo::print_genes(){
 
 class Population{
 	public:
-		Population();
-		~Population();
+		Population(); //construtor padrão
+		~Population(); //destrutor padrão
 		void printPopulation();
+		int index_best_score();
+
 
 	//private:
 		vector<Individuo> population;
@@ -107,6 +124,18 @@ Population::~Population(){
 	;
 }
 
+int Population::index_best_score(){
+	int best_score = 0;
+
+	for (int i = 0; i < tam_pop; ++i){
+		//if( (this->population[best_score]).score < (this->population[i]).score )
+		if(this->population[best_score] < this->population[i])
+			best_score = i;
+	}
+
+	return best_score;
+}
+
 void Population::printPopulation(){
 	for (int i = 0; i < tam_pop; ++i){
 		cout << "Individuo " << i + 1 << " :\n";
@@ -116,11 +145,64 @@ void Population::printPopulation(){
 }
 
 
-
-
 int main(int argc, char const *argv[]){
 	srand(time(NULL));
-	Population especimes;
+	Population especimes;//constroi população
 	especimes.printPopulation();
+
+	//cout << especimes.index_best_score() + 1;
+
+	for (int i = 0; i < geracoes; ++i){
+
+		for(int j = 0; j < tam_torneio; j++){
+			
+			double prob = ((double) rand() / ((double)RAND_MAX + 1) );
+			
+			if(prob < prob_cruz){
+				int indice_pai1 = rand() % tam_pop;
+				
+				int indice_pai2 = rand() % tam_pop;
+				
+				//garante que os indices são diferentes
+				while(indice_pai2 == indice_pai1){
+					indice_pai2 = rand() % tam_pop;
+				}
+				Individuo filho;
+
+				//filho.print_genes();
+				filho.crossing(especimes.population[indice_pai1], especimes.population[indice_pai2]);
+				filho.at_score();//atualiza o score
+				//filho.print_genes();
+
+				double prob = ((double) rand() / ((double)RAND_MAX + 1) );
+
+				if(prob < prob_mut){
+					filho.mutation();
+					filho.at_score();//atualiza o score
+				}
+
+				if( especimes.population[indice_pai1] < filho){
+					//copia genes do filho para o pai
+					for (int k = 0; k < tam_genes; ++k){
+						especimes.population[indice_pai1].genes[k] = filho.genes[k];
+					}
+					especimes.population[indice_pai1].at_score();//atualiza o score
+
+				}
+			}
+		}
+		cout << "Geracao " << i+1 << endl;
+
+		int index = especimes.index_best_score();
+		cout << "Genes: ";
+		especimes.population[index].print_genes();
+		cout << endl;
+
+		if(especimes.population[index].score == tam_genes)
+			break;	
+
+	}
+
+
 	return 0;
 }
