d4f8c9ead96ba8a5d19d90302a0751674dd0e45d
victorlelissoares <victorlelis2011@gmail.com>
2022-09-19 00:07:43 -0400

    Implementing limits of routes

--- vrp_/automateTest.sh
+++ vrp_/automateTest.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 #benchmarkName num_test
 g++ *.cpp -o vrp_simple -Wall -O3 
-value = '0'
+
 
 #executa n vezes o programa
-for i in {0..($2-1)}
+for i in {0..$3-1}
     do
-    value = value + './vrp_simple benchmarks/$1'#recebe a saida do programa, que é o fit
+    ./vrp_simple benchmarks/$1 $2 $3 #recebe a saida do programa, que é o fit
 done
 
-#faz a média das distâncias
-value = value / $2
-
-echo "Distância média percorrida: $value\n"
 
 
--- vrp_/individuo.hpp
+++ vrp_/individuo.hpp
@@ -29,6 +29,9 @@ double prob_mut = 0.8; // probabilidade de mutação
 
 double prob_cruz= 0.6; // probabilidade de cruzamento
 
+double prob_repair = 0.1;//como a reparação de cromosso é um processo muito custoso, a probabilidade de acontecer
+//será baixa para garantir o bom desempenho do programa
+
 int selec_gene   = 60;
 
 int capacity      = 0;
@@ -48,26 +51,26 @@ class Individuo{
 		void atScore();
 		int returnScore();
 		void printGenes();
-		
-
-	//private:
-		//first correponde ao número do cliente e second a chave aleatória
-		vector< pair<int, double> > cromossomo;//vetor de genes
-		double score;//score do individuo, vai ser calculado através da distância euclidiana+penalidade
-		double real_score;
-		int infeasibility;
 		void mutation(void (*type_mutation)(Individuo *indi));//mutação
 		void crossing(Individuo *pai1, Individuo *pai2, 
 		void (*type_crossing)(Individuo*, Individuo*, Individuo*));//operador de cruzamento, recebe a função de cruzamento como parâmetro
+
+		//first correponde ao número do cliente e second a chave aleatória
+		vector< pair<int, double> > cromossomo;//vetor de genes
+		vector<pair<int, int>> limits_route;//diz onde começa e termina uma rota de um K dentro do cromossomo
+		double score;//score do individuo, vai ser calculado através da distância euclidiana+penalidade
+		double real_score;
+		int infeasibility;
+		
 };
 
 void Individuo::atScore(){
 	infeasibility = 0;
 	int peso_penality   = 0;
-	vector<pair<int, double>> v = this->cromossomo;
+	//vector<pair<int, double>> v = this->cromossomo;
 	// Como a ordem de visitas de um veículo é dado pelo seu double
 	// ordeno o vetor para saber qual a ordem de visitas aos clientes
-	sort(v.begin(), v.end(),
+	sort(this->cromossomo.begin(), this->cromossomo.end(),
      [](const pair<int, double>& lhs, const pair<int, double>& rhs) {
              return lhs.second < rhs.second; } );
 	
@@ -75,29 +78,36 @@ void Individuo::atScore(){
 	double score_fit = 0;
 	int i = 0;
 	int indi_1 = 0;
-	int indi_2 = v[i].first-1;
-	int actual_car = 1;//considerando que existe mais de um veículo
+	int indi_2 = this->cromossomo[i].first-1;
+	int actual_car = 1;//considerando que existe mais de um this.cromossomoeículo
 	int peso = 0;
 	
-	//cout << "veículo " << (int) v[i].second << " saindo do depósito" << endl;
-
+	//cout << "this.cromossomoeículo " << (int) this.cromossomo[i].second << " saindo do depósito" << endl;
+	int init_route = i, final_route;
+	
 	while ( i < tam_genes){
+		//cout << "i: "<<  i << endl;
 		score_fit +=  distEuclidiana(distance_vec[indi_1], distance_vec[indi_2]);
 		
 		indi_1 = indi_2;
 		peso += demand_vec[indi_1];
+		
 		i++;
-		indi_2 = v[i].first-1;
-
+		indi_2 = this->cromossomo[i].first-1;
+		//cout << "indi2: " << indi_2;
 		//significa que trocou de carro
 		//ou seja, outra rota
 		//então deve-se retornar ao depósito
-		if(actual_car < (int) v[i].second){
-			
-			actual_car = (int) v[i].second;
+		if(actual_car < (int) this->cromossomo[i].second){
+			//cout << "troca carro" << endl;
+			actual_car = (int) this->cromossomo[i].second;
 			
 			score_fit +=  distEuclidiana(distance_vec[indi_1], distance_vec[0]);//retorna ao depósito
 			
+			final_route = i - 1;
+			//cout << "ini: " << init_route << " fi: " << final_route << endl;
+			limits_route.push_back(make_pair(init_route, final_route));
+			init_route = i;
 			
 			//após retornar o depósito, checamos se aquela rota
 			// não excedeu o quanto o caminhão pode levar
@@ -116,12 +126,19 @@ void Individuo::atScore(){
 			peso = 0;
 			peso += demand_vec[indi_2];
 			indi_1 = indi_2;
+			//cout << "i: "<<  i << endl;
 			i++;
-			indi_2 = v[i].first-1;
+			//cout << "i: "<<  i << endl;
+			indi_2 = this->cromossomo[i].first-1;
 		}
-		if(indi_2+1 == 0){//fim da linha
+		//cout << "i: "<<  i << endl;
+		if(i >= tam_genes){//fim da linha
+			//cout << "cabo porra" << endl;
 			// quer dizer que terminaram as entregas
 			// então o caminhão atual deve retornar ao depósito
+			final_route = i-1;
+			//cout << "ini: " << init_route << " fi: " << final_route << endl;
+			limits_route.push_back(make_pair(init_route, final_route));
 
 			score_fit += distEuclidiana(distance_vec[indi_1], distance_vec[0]);
 			if(peso > capacity){//significa que a capacidade do caminhão foi ultrapassada
@@ -151,7 +168,16 @@ Individuo::Individuo(): score(0), real_score(0), infeasibility(0){
 
 		this->cromossomo.push_back(make_pair(j++, gene));//insere o gene no cromossomo
 	}
-	this->atScore(); // atualiza o score do individuo
+	
+	double prob = fRand(0, 1);
+	
+	// if(prob_repair > prob){//então tem que reparar o cromossomo
+	// 	this->atScore(1); // atualiza o score do individuo
+	// }
+	// else{
+	// 	this->atScore(0); // atualiza o score do individuo
+	// }
+	atScore();
 }
 
 Individuo::~Individuo(){
