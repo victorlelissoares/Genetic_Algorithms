fa629ef74f72716999f2237ff8a772586b552b7e
Victor Lelis <victor.soares@acto.com.br>
2023-08-16 21:51:53 -0400

    Criando bash script para automatização de testes

--- vrp_/automateTest.sh
+++ vrp_/automateTest.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+#set -x
 
-#ainda não terminado
-g++ *.cpp -o vrp_simple -O3 
+g++ main.cpp -o vrp -O3
 
-
-#executa n vezes o programa
-for (( i = 1; i <= $1; i++))
-    do
-    value_obtined = $(./vrp_simple benchmarks/$2 $3) #recebe a saida do programa, que é o fit
+# Executa n vezes o programa
+for ((i = 1; i <= $1; i++)); do
     
-    gap = $[[100 * [$4 - $value_obtined] / $4] + $gap]
+    #cria arquivo com o nome da instância com extensão out
+    namefile="${2:2}"
+    namefile="${namefile::-4}"
+    namefile="${namefile}.out"
+    
+    #executando script
+    output=$(./vrp benchmarks/$2 $3)
+
+    #imprimindo saída no console e no arquivo de saída
+    echo $output | tee -a "results/${namefile}"
+    
+    #sleep de 5 segundos para não ter problema na geração de números aleatórios
+    sleep 5
 done
-
-
-
-
-
